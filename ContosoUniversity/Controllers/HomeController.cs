24cb1ecbbc3afc9dfebf36d209fbe85b61d04307
DESKTOP-OG16UI9\james-nhan-do <nhan.ttdo@gmail.com>
2022-03-08 14:01:19 -0600

    Add project files.

---
+++ ContosoUniversity/Controllers/HomeController.cs
@@ -0,0 +1,23 @@
+﻿using ContosoUniversity.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace ContosoUniversity.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public ActionResult Index()
+        {
+            ViewBag.Message = "Welcome to Contoso University";
+
+            return View();
+        }
+    }
+}
